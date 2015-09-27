a9bd450d99fd90278dfda17ce87fbc78bfc369dc
KeyboardFire <andy@keyboardfire.com>
2015-09-27 10:36:39 -0500

    better format in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,21 @@ use macaroni_lang::*;
 
 fn main() {
     let mac = macaroni::Macaroni::new();
-    mac.run("print tobase add 1 1 10 print \"\n\"".to_string());
-    mac.run("print tobase multiply 5 5 10 print \"\n\"".to_string());
-    mac.run("print tobase pow 2 16 10 print \"\n\"".to_string());
-    mac.run("print tobase pow 2 -1 10 print \"\n\"".to_string());
-    mac.run("print tobase multiply pow 2 -1 100 10 print \"\n\"".to_string());
-    mac.run("print tobase multiply floor pow 2 -1 100 10 print \"\n\"".to_string());
-    mac.run("print wrap 33 print wrap 10".to_string());
-    mac.run("print tobase rand 10 print \"\n\"".to_string());
-    mac.run("print \"Hello, World!\n\"".to_string());
-    mac.run("print tobase asdf 10 print \" = 0\n\"".to_string());
+    for test in r#"
+            print tobase add 1 1 10
+            print tobase multiply 5 5 10
+            print tobase pow 2 16 10
+            print tobase pow 2 -1 10
+            print tobase multiply pow 2 -1 100 10
+            print tobase multiply floor pow 2 -1 100 10
+            print wrap 33
+            print tobase rand 10
+            print "Hello, World!"
+            print tobase asdf 10 print " = 0"
+            "#.lines() {
+        if !test.chars().all(char::is_whitespace) {
+            mac.run(test.to_string());
+            println!("");
+        }
+    }
 }
